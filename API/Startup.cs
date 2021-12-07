12366dc722b3b6d185d6166b4a10f44c4b9f65e3
Sid <sid_sid@hotmail.com>
2021-12-07 12:51:10 +0530

    Fixed CORS issue.

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,23 +33,25 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
         }
 
-        public void ConfigureDevelopmentServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(options => {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-        }
+        // public void ConfigureDevelopmentServices(IServiceCollection services)
+        // {
+        //     services.AddDbContext<DataContext>(options => {
+        //         options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+        //     });
+        //     services.AddControllers();
+        //     services.AddCors();
+        //     services.AddSwaggerGen(c =>
+        //     {
+        //         c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+        //     });
+        // }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -64,6 +66,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => { x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");});
 
             app.UseAuthorization();
 
